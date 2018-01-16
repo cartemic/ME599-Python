b3235d8a1cc0d368622f6513e1ab8379ad2bbb1e
Mick <cartemic@oregonstate.edu>
2018-01-16 12:50:30 -0800

    finished close.py

---
+++ Lab1/close.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""
+Created on Tue Jan 16 12:43:35 2018
+
+@author: Mick
+"""
+
+import numpy as np
+
+
+def close(num1, num2, num3):
+    """
+    Takes three numbers as arguments and returns True if the absolute
+    difference between the first two numbers is less than the third number.
+    """
+    if np.abs(num1 - num2) < num3:
+        return(True)
+    else:
+        return(False)
+
+
+if __name__ == "__main__":
+    # test stuff
+    print(close(1, 2, 3))
+    print(close(1, 7, 1))
--- Lab1/volumes.py
+++ Lab1/volumes.py
@@ -7,12 +7,43 @@ Created on Tue Jan 16 12:06:38 2018
 
 import numpy as np
 
+
 def cylinder_volume(radius, height):
-# calculate cylinder volume from user inputs
-    return(np.pi * radius**2 * height)
+    """
+    calculates cylinder volume from user inputs
+    """
+
+    # ensure inputs aren't dumb
+    if (radius < 0) or (height < 0):
+        return(None)
+    else:
+        return(np.pi * radius**2 * height)
+
+
+def torus_volume(majorRadius, minorRadius):
+    """
+    calculates torus volume from user inputs
+    """
+
+    # ensure inputs aren't dumb
+    if (majorRadius < 0) or (minorRadius < 0):
+        return(None)
+    else:
+        return((np.pi * minorRadius**2) * (2. * np.pi * majorRadius))
+
 
 if __name__ == "__main__":
-    # enter test conditions
-    height = 5.
-    radius = 3.
-    print(cylinder_volume(radius, height))
+    # test cylinder for regular stuff and negative numbers
+    print('cylinder test\n=============')
+    height = 5. * np.array([1, 1, -1])
+    radius = 3. * np.array([1, -1, 1])
+    print([cylinder_volume(radius[i], height[i]) for i in range(len(height))])
+    print('\n')
+
+    # test torus regular stuff and negative numbers
+    print('torus test\n==========')
+    minorRadius = 3. * np.array([1, 1, -1])
+    majorRadius = 4. * np.array([1, -1, 1])
+    print([cylinder_volume(majorRadius[i], minorRadius[i]) for i in
+           range(len(majorRadius))])
+    print('\n')
