4f7d5c4f205885264b36046eda62ad9f42b6fd3e
Mick <cartemic@oregonstate.edu>
2018-01-16 13:04:22 -0800

    words.py

---
+++ Lab1/words.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+"""
+Created on Tue Jan 16 12:51:36 2018
+
+@author: Mick
+"""
+
+import string as st
+
+
+def letter_count(input_string, letter_to_count):
+    """
+    Takes two string arguments: the first string is a bunch of text, and the
+    second is a letter.  The function returns the number of occurrences of the
+    letter in the text, and is case insensitive.
+    """
+    input_string = st.lower(input_string)
+    letter_to_count = st.lower(letter_to_count)
+    return(st.count(input_string, letter_to_count))
+
+
+if __name__ == "__main__":
+    # test stuff
+    test_string = 'woo woo AWOOGA'
+    print(letter_count(test_string, 'w'))
+    print(letter_count(test_string, 'z'))
