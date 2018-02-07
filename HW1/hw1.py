e132f7e3279a70701aa34c7e062a6eb96e8a13e7
Mick Carter <cartemic@oregonstate.edu>
2018-02-06 18:37:39 -0800

    homework 1 parts 2 and 3 finished

--- HW1/hw1.py
+++ HW1/hw1.py
@@ -6,12 +6,58 @@ Created on Mon Feb 05 16:55:11 2018
 """
 
 import csv
+import numpy as np
+
+
+def assignment_scores(data, assignment_number):
+    scores = np.array([])
+    assignment_name = data[0][assignment_number]
+    # collect scores from all students
+    for i in xrange(len(data)-1):
+        try:
+            # add score to numpy array as a float rather than a string
+            scores = np.append(scores,
+                               float(data[i+1][assignment_number]))
+        except ValueError:
+            # get rid of those pesky 'EX' scores
+            pass
+    return assignment_name, scores
+
+
+def hardest_assignment(data):
+    num_assignments = len(data[0])-1
+    assignments = dict()
+
+    # organize assignments into a dictionary, removing any with all-zero scores
+    for i in xrange(num_assignments):
+        name, scores = assignment_scores(data, i+1)
+        if max(scores) > 0:
+            assignments[name] = np.mean(scores)
+
+    # find and return hardest assignment
+    return assignments.keys()[np.argmin(assignments.values)]
+
 
 with open('grades.csv', 'r') as f:
     reader = csv.reader(f)
-    grades = list(reader)
+    data = list(reader)
 
-assignments = grades[0][1:]
-grades = grades[1:]
+final_scores = assignment_scores(data, len(data[0])-1)[1]
+final_avg = np.mean(final_scores)
+final_med = np.median(final_scores)
 
-for i, g in enumerate(grades)
+print('Average Score: {0:.2f}'.format(final_avg))
+print('Above Average: {0:.2f}%'.format(100 * sum(final_scores
+                                                 >
+                                                 final_avg
+                                                 )/float(len(final_scores))))
+print('Median Score: {0:.2f}'.format(final_med))
+print('Above Median: {0:.2f}%'.format(
+                                      100 * sum(final_scores
+                                                >
+                                                final_med
+                                                )/float(len(final_scores))))
+print('Hardest Assignment: ' + hardest_assignment(data))
+
+
+#for i, g in enumerate(grades)
