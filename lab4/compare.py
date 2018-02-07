012bd54e65cfd78c8e66a34f999c577d29fac574
Mick Carter <cartemic@oregonstate.edu>
2018-02-06 16:53:47 -0800

    it works, lab4 complete assuming the only/both words are supposed to be unique

--- lab4/compare.py
+++ lab4/compare.py
@@ -11,6 +11,10 @@ import string
 
 
 def comparison(my_file):
+    """
+    Opens a file and counts the number of total and unique words in it
+    output: (# total words, # unique words, set of unique words)
+    """
     word_count = 0
     words = set()
     with open(my_file, 'r') as f:
@@ -23,15 +27,7 @@ def comparison(my_file):
                           .split()
             [words.add(w) for w in line_words]
             word_count += len(line_words)
-    return word_count, len(words)
-
-
-def words_only_in(my_file):
-    return None
-
-
-def words_in_both(my_files):
-    return None
+    return word_count, len(words), words
 
 
 if len(sys.argv) < 3:
@@ -39,7 +35,7 @@ if len(sys.argv) < 3:
     print('Two arguments is the minimum requirement for a nutritious function')
 elif len(sys.argv) > 3:
     # user has given too many arguments
-    print('I\'m ignoring everything but your first two arguments')
+    print('That\'s too many arguments! AUGH! MY PROCESSOR!')
 else:
     # user has given the correct amount of arguments
     files = [sys.argv[1], sys.argv[2]]
@@ -48,19 +44,44 @@ else:
     exist_1 = os.path.isfile(files[0])
     exist_2 = os.path.isfile(files[1])
     if exist_1 and exist_2:
-        file_counts = [comparison(f) for f in files]
+        file_info = [comparison(f) for f in files]
+        """
+        file_info is a list of tuples:
+            (total words, unique words, set of unique words)
+        """
+
         for i, f in enumerate(files):
             print(f)
-            print('    {0} words'.format(file_counts[i][0]))
-            print('    unique: {0}'.format(file_counts[i][1]))
+            print('    {0} words'.format(file_info[i][0]))
+            print('    unique: {0}'.format(file_info[i][1]))
 
-        for f in files:
-            print('Only {0:s}: {1}'.format(f, words_only_in(f)))
+        # find out how many words are shared by the datasets
+        shared = len(set.intersection(file_info[0][2], file_info[1][2]))
 
-#        print('Both files:',words_in_both(files))
+        for i, f in enumerate(files):
+            print('Only {0:s}: {1}'.format(f, len(file_info[i][2]) - shared))
+
+        print('Both files: {0}'.format(shared))
     elif not exist_1 and not exist_2:
-        print('Both of your filenames are jacked up.')
+        print('Both of your filenames are jacked up. Get it together!')
     elif not exist_1:
-        print('Invalid first filename.')
+        print('Invalid first filename. Have you been drinking?')
     else:
-        print('Invalid second filename.')
+        print('Invalid second filename. Try harder.')
+
+"""
+DEBUGGING CHECK:
+
+test.txt
+    9 words
+    unique: 9
+test2.txt
+    19 words
+    unique: 17
+Only test.txt 1
+Only test2.txt 9
+Both files: 8
+
+
+total wall time for war and peace and words comparison ~750ms << 10s :)
+"""
