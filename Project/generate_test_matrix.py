fe57b03acfdadb0fe5f0510c8a41b4117603b61f
Mick Carter <cartemic@oregonstate.edu>
2018-03-13 17:02:04 -0700

    Replicates now created by randomizing initial test run

--- Project/generate_test_matrix.py
+++ Project/generate_test_matrix.py
@@ -48,7 +48,7 @@ def Output_results(dataframe_list):
         file_name = 'Test_matrix_replicate_' + str(i+1) + '.csv'
         if os.path.exists(file_name):
             os.remove(file_name)
-        df.to_csv(file_name)
+        df.to_csv(file_name, index=False)
 
 
 def Generate(initial_pressure,
@@ -65,8 +65,8 @@ def Generate(initial_pressure,
     initial_temperature = det.Temperature(initial_temperature)
 
     # make sure there is an undiluted case
-    if None not in diluent_str_list:
-        diluent_str_list.append(None)
+    if 'None' not in diluent_str_list:
+        diluent_str_list.append('None')
     diluent_str_list = sorted(diluent_str_list)
 
     # initialize a detonation
@@ -86,60 +86,77 @@ def Generate(initial_pressure,
     df_list = []
     for replicate in xrange(replicates):
         print 'replicate', replicate
-        # initialize blank list of test conditions
         df = pd.DataFrame(columns=column_titles)
+        # initialize blank list of test conditions
+        if replicate == 0:
+            """REMOVE"""
+            pass
+            # block by diluent
+            for i in xrange(len(diluent_str_list)):
+                print '    diluent', i
+                # build list of all test combinations
+                test_conditions = list(itertools.product(*[equivalence_list,
+                                                         [diluent_str_list[i]],
+                                                         diluent_mf_list]))
 
-        # block by diluent
-        for i in xrange(len(diluent_str_list)):
-            print '    diluent', i
-            # build list of all test combinations
-            test_conditions = list(itertools.product(*[equivalence_list,
-                                                       [diluent_str_list[i]],
-                                                       diluent_mf_list]))
+                for j in xrange(len(test_conditions)):
+                    print '        test', j
+                    # select a random test condition
+                    [equivalence, diluent, mf] = test_conditions.pop(
+                            random.randrange(len(test_conditions)))
+                    data = [diluent, equivalence, mf,
+                            [], [], [], [], [], [], []]
 
-            for j in xrange(len(test_conditions)):
-                print '        test', j
-                # select a random test condition
-                [equivalence, diluent, mf] = test_conditions.pop(
-                        random.randrange(len(test_conditions)))
-                data = [diluent, equivalence, mf, [], [], [], [], [], [], []]
+                    # update detonation for each diluent, equivalence, and mf
+                    if diluent_str_list[i] is 'None':
+                        # undiluted case
+                        diluted = False
+                        pass
 
-                # update detonation for each diluent, equivalence, and mf
-                if diluent_str_list[i] is None:
-                    # undiluted case
-                    diluted = False
-                    pass
+                    else:
+                        # diluted case
+                        my_detonation.set_equivalence(equivalence)
+                        my_detonation.add_diluent(diluent, mf)
+                        diluted = True
 
-                else:
-                    # diluted case
-                    my_detonation.set_equivalence(equivalence)
-                    my_detonation.add_diluent(diluent, mf)
-                    diluted = True
+                    # calculate partial pressures of each component. Filling
+                    # will be done fuel -> oxidizer -> diluent, so partials
+                    # will be added in this order in order to get cutoff
+                    # pressures for each component.
+                    pressures = my_detonation.get_pressures(diluted)
+                    data[3] = pressures[fuel_string]
+                    data[4] = data[3] + pressures[oxidizer_string]
+                    try:
+                        data[5] = data[4] + pressures[diluent_str_list[i]]
+                    except:
+                        data[5] = 'N/A'
 
-                # calculate partial pressures of each component. Filling will
-                # be done fuel -> oxidizer -> diluent, so partials will be
-                # added in this order in order to get cutoff pressures for
-                # each component.
-                pressures = my_detonation.get_pressures(diluted)
-                data[3] = pressures[fuel_string]
-                data[4] = data[3] + pressures[oxidizer_string]
-                try:
-                    data[5] = data[4] + pressures[diluent_str_list[i]]
-                except:
-                    data[5] = 'N/A'
+                    # calculate cj
+                    data[6] = my_detonation.CJ_Velocity(diluted).value
+                    # calculate mass used
+                    masses = my_detonation.get_mass(tube_volume_m3, diluted)
+                    data[7] = masses[fuel_string]
+                    data[8] = masses[oxidizer_string]
+                    try:
+                        data[9] = masses[diluent_str_list[i]]
+                    except:
+                        data[9] = 0.
+                    # put into sheet
+                    df.add_row(data)
 
-                # calculate cj
-                data[6] = my_detonation.CJ_Velocity(diluted).value
-                # calculate mass used
-                masses = my_detonation.get_mass(tube_volume_m3, diluted)
-                data[7] = masses[fuel_string]
-                data[8] = masses[oxidizer_string]
-                try:
-                    data[9] = masses[diluent_str_list[i]]
-                except:
-                    data[9] = 0.
-                # put into sheet
-                df.add_row(data)
+        else:
+            # randomize first dataframe
+            # loop through blocked diluents
+            for i, diluent in enumerate(diluent_str_list):
+                # separate out current diluent
+                sub_df = df_list[0][df_list[0].Diluent == diluent]
+
+                # randomize this block and add it to the current dataframe
+                new_order = [int(idx) for idx in sub_df.index]
+                random.shuffle(new_order)
+                df = pd.concat([df, sub_df.reindex(new_order)])
+
+        # append current dataframe to list
         df_list.append(df)
     return df_list
 # %% main program
@@ -154,9 +171,9 @@ if __name__ == '__main__':
     tube_volume_m3 = 1.
     fuel = 'H2'
     oxidizer = 'O2'
-    diluents = ['AR', 'N2', 'CO2']
+    diluents = ['AR']
     equivalence = [0.75, 1., 1.25]
-    diluent_mf = [0.1, 0.2, 0.3]
+    diluent_mf = [0.1]
     replicates = 4
     test = Generate(P0, T0, fuel, oxidizer, diluents, equivalence, diluent_mf,
                     tube_volume_m3, replicates)
